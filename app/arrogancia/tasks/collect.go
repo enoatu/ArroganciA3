84362b221e5f51068c09e9ffb7b7de8c2f97b1c8
enoatu <acro1452@gmail.com>
2021-07-12 23:51:53 +0900

    :wrench: filter tweets

--- app/arrogancia/tasks/collect.go
+++ app/arrogancia/tasks/collect.go
@@ -6,8 +6,6 @@ import (
 	"github.com/beego/beego/v2/adapter/logs"
 	"github.com/dghubble/go-twitter/twitter"
 	"os"
-	// "net/url"
-	// "unicode/utf8"
 	"strconv"
 	"strings"
 )
@@ -26,15 +24,10 @@ func Collect() {
 	greedWordsQueryStr := greedWords{}.get().getQueryStr()
 	query := fmt.Sprintf("%s exclude:retweets exclude:replies filter:safe", greedWordsQueryStr)
 	logs.Warn(query)
-	// API仕様としてクエリは500文字以内?
-	// if length := utf8.RuneCountInString(url.QueryEscape(query)); length > 1000 {
-	// 	logs.Warn(fmt.Sprintf("query string length(%s) is over 1000", strconv.Itoa(length)))
-	// 	return
-	// }
 	searchTweetParams := &twitter.SearchTweetParams{
 		Query:     query,
 		TweetMode: "extended",
-		Count:     3,
+		Count:     50,
 	}
 
 	search, _, err := client.Search.Tweets(searchTweetParams)
@@ -43,9 +36,12 @@ func Collect() {
 		return
 	}
 	services.P(search.Statuses)
-	for _, v := range search.Statuses {
-		fmt.Printf("SEARCH TWEETS:\n%+v\n", v.FullText)
+	tweets := filterTweets(search.Statuses)
+	for _, v := range tweets {
+		fmt.Printf("SEARCH TWEETS:\n%+v\n", v)
 	}
+	lastTweet := tweets[len(tweets)-1]
+	logs.Info(lastTweet)
 	// fmt.Printf("SEARCH METADATA:\n%+v\n", search.Metadata)
 }
 
@@ -87,21 +83,15 @@ func (gs greedWords) getQueryStr() (queryStr string) {
 	return
 }
 
-// func (statues search.Statuses) filterWords (search.Statuses) {
-// 	ngWordCountStr := os.Getenv("EXCEPT_WORDS_COUNT")
-// 	ngWordCount, err := strconv.Atoi(ngWordCountStr)
-// 	if err != nil {
-// 		logs.Warn(err)
-// 		return ns
-// 	}
-// 	i := 0
-// 	for i < ngWordCount {
-// 		ngWordRawStrs := os.Getenv(fmt.Sprintf("NG_WORDS_%s", strconv.Itoa(i)))
-// 		logs.Warn(ngWordRawStrs)
-// 		logs.Warn(fmt.Sprintf("NG_WORDS_%s", i))
-// 		ngWordRaws := strings.Split(ngWordRawStrs, ",")
-// 		ns = append(ns, ngWordRaws...)
-// 		i++
-// 	}
-// 	return ns
-// }
+func filterTweets(tweets []twitter.Tweet) (filteredTweets []twitter.Tweet) {
+	ns := ngWords{}.get()
+	for _, tweet := range tweets {
+		for _, ngWord := range ns {
+			if strings.Contains(tweet.FullText, ngWord) {
+				break
+			}
+		}
+		filteredTweets = append(filteredTweets, tweet)
+	}
+	return
+}
