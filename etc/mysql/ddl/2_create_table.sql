daa44e9f651dd6abcb3495c429a04cc554723de5
enoatu <acro1452@gmail.com>
2021-07-14 08:14:50 +0900

    :wrench: up db

--- etc/mysql/ddl/2_create_table.sql
+++ etc/mysql/ddl/2_create_table.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS tweet (
   id                INT UNSIGNED NOT NULL AUTO_INCREMENT                                     COMMENT 'ID',
   tweet_id          INT UNSIGNED NOT NULL                                                    COMMENT 'tweetのID',
   search_word_id    INT UNSIGNED NOT NULL                                                    COMMENT '検索ワードID',
-  text              TEXT NOT NULL                                                            COMMENT 'ツイート本文'
-  user_name         VARCHAR(255) NOT NULL                                                    COMMENT 'ユーザーネーム'
-  user_screen_name  VARCHAR(255) NOT NULL                                                    COMMENT 'ユーザースクリーンネーム'
+  text              TEXT NOT NULL                                                            COMMENT 'ツイート本文',
+  user_name         VARCHAR(255) NOT NULL                                                    COMMENT 'ユーザーネーム',
+  user_screen_name  VARCHAR(255) NOT NULL                                                    COMMENT 'ユーザースクリーンネーム',
   created_at        DATETIME NOT NULL                                                        COMMENT 'ツイート作成日時',
   created_on        DATETIME NOT NULL                                                        COMMENT '作成日時',
   modified_on       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最終更新日時',
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS tweet (
 
 CREATE TABLE IF NOT EXISTS search_word (
   id               INT UNSIGNED NOT NULL AUTO_INCREMENT                                     COMMENT 'ID',
-  word             VARCHAR(255) NOT NULL                                                    COMMENT '検索ワード'
+  word             VARCHAR(255) NOT NULL                                                    COMMENT '検索ワード',
   created_on       DATETIME NOT NULL                                                        COMMENT '作成日時',
   modified_on      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最終更新日時',
   PRIMARY KEY (id)
