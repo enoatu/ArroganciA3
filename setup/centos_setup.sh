d27fb767e55fed668b2a5c9355bc09b4c0de6d7e
enoatu <acro1452@gmail.com>
2021-07-29 00:03:09 +0900

    :wrench: move setup

---
+++ setup/centos_setup.sh
@@ -0,0 +1,23 @@
+#!/usr/local/env bash
+
+<< COMMENTOUT
+sudo yum update -y && sudo yum install -y git && git clone https://github.com/enoatu/setup; ./setup/centos_setup.sh && git clone https://github.com/enoatu/ArroganciA3; ./ArroganciA3/setup/centos_setup.sh
+COMMENTOUT
+
+# amazon linux2
+# 1. install docker
+sudo amazon-linux-extras install docker
+# 2. start service
+sudo service docker start
+# 3. enable no-sudo
+sudo usermod -a -G docker ec2-user
+# 4. install docker-compose and enable no-sudo
+sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
+# 5. yarn last setup
+sudo curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
+sudo yum install -y nodejs
+exsh
+sudo npm install -g yarn
+exsh
+cd ArroganciA3
+yarn install
---
+++ setup/debian_setup.sh
@@ -0,0 +1,38 @@
+#!/usr/local/env bash
+
+<< COMMENTOUT
+sudo apt update -y && sudo apt install -y git && git clone https://github.com/enoatu/setup; ./setup/debian_setup.sh && git clone https://github.com/enoatu/ArroganciA3; ./ArroganciA3/setup/debian_setup.sh
+COMMENTOUT
+
+# debian
+# 1. install docker
+# ref: https://matsuand.github.io/docs.docker.jp.onthefly/engine/install/debian/
+sudo apt update -y
+sudo apt install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+ echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update -y
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+# 2. start service
+sudo systemctl enable docker
+sudo systemctl restart docker
+# 3. enable no-sudo
+sudo usermod -a -G docker $USER
+# 4. install docker-compose and enable no-sudo
+sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
+# 5. yarn last setup
+sudo curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
+#sudo apt install -y nodejs
+#sudo npm install -g yarn
+#cd ArroganciA3
+#yarn install
