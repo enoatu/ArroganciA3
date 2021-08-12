fa8867cc0b4dd11908757310dd1119b8639c95f1
enoatu <acro1452@gmail.com>
2021-08-12 14:08:42 +0900

    :wrench: modify encrypt

--- misc/env/encrypt.sh
+++ misc/env/encrypt.sh
@@ -2,6 +2,8 @@
 
 # not libressl, require openssl
 read -s -p "Enter Password: " PASS1
+echo
 read -s -p "ReEnter Password: " PASS2
-[$PASS1 != $PASS2] && echo "mismatched password" && exit 1
-docker-compose exec -T app bash -c "cd /go/src/arrogancia && openssl enc -e -aes-256-cbc -md sha256 -iter 10000 -salt -k $PASS -in .env -out .env.encrypt"
+echo
+[ $PASS1 != $PASS2 ] && echo "mismatched password" && exit 1
+docker-compose exec -T app bash -c "cd /go/src/arrogancia && openssl enc -e -aes-256-cbc -md sha256 -iter 10000 -salt -k $PASS1 -in .env -out .env.encrypt"
