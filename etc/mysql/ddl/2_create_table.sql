fbab06db9fb21856c3b59c8c2b53866114a98e21
enoatu <acro1452@gmail.com>
2021-08-13 18:02:05 +0900

    :heavy_plus_sign: add index

--- etc/mysql/ddl/2_create_table.sql
+++ etc/mysql/ddl/2_create_table.sql
@@ -11,7 +11,8 @@ CREATE TABLE IF NOT EXISTS tweet (
   created_on        DATETIME NOT NULL                                                        COMMENT '作成日時',
   modified_on       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最終更新日時',
   PRIMARY KEY (id),
-  KEY tweet_index01 (search_word_id, optimize_level)
+  KEY tweet_index01 (search_word_id, optimize_level),
+  KEY tweet_index02 (user_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='ツイート';
 
 CREATE TABLE IF NOT EXISTS search_word (
